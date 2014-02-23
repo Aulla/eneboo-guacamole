bf28a02906702834fb14433fa224f2ee3ab6517f
José A. Fernández <aullasistemas@gmail.com>
2014-02-23 20:52:11 +0100

    Servidor: Algunas correcciones del instalador

--- interface/final/instalar.sh
+++ interface/final/instalar.sh
@@ -3,10 +3,10 @@ echo Instalando tomcat , vnc y guacamole
 apt-get install tomcat6 libvncserver0 openjdk-6-jdk vnc4server fluxbox guacamole guacd libguac-client-vnc0 libguac3 guacamole-tomcat
 echo Compilando WAR
 cd ../guaca-src
-jar cvf guaca.war .
+jar cvf guacamole.war .
 echo Borrando WAR viejo
-rm ../final/guaca.war
-cp guaca.war ../final
+rm ../final/guacamole.war
+cp guacamole.war ../final
 cd ../final
 echo Listo , ahora a instalar
 
@@ -41,13 +41,13 @@ echo Listo , ahora a instalar
 #cp ./guacamole.properties /etc/guacamole/
 #cp ./user-mapping.xml /etc/guacamole/
 chmod 777 /etc/guacamole/user-mapping.xml
-cp ./guaca.war /var/lib/guacamole/guaca.war
-ln -s /var/lib/guacamole/guaca.war /var/lib/tomcat6/webapps/guaca.war
+cp ./guacamole.war /var/lib/guacamole/guacamole.war
+ln -s /var/lib/guacamole/guacamole.war /var/lib/tomcat6/webapps/guacamole.war
 ln -s /etc/guacamole/guacamole.properties /var/lib/tomcat6/common/classes/
 echo Reiniciando Tomcat
 /etc/init.d/tomcat6 restart
 echo cambiando permisos en traspaso
-mkdir /var/lib/tomcat6/webapps/guaca/traspaso
-chmod -R 777 /var/lib/tomcat6/webapps/guaca/traspaso
+mkdir /var/lib/tomcat6/webapps/guacamole/traspaso
+chmod -R 777 /var/lib/tomcat6/webapps/guacamole/traspaso
 echo Listo. Ahora ejecute uguaca.sh con cada usuario a usar.
 echo Listo ...
